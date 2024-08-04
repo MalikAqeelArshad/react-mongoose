c18759368f65e00ccf577359905f24e423dd126a
Aqeel Malik <malik.aqeelarshad@gmail.com>
2024-08-05 01:44:29 +0400

    vercel api path updated

--- client/src/components/JobListings.jsx
+++ client/src/components/JobListings.jsx
@@ -3,7 +3,7 @@ import JobListing from './JobListing';
 import Spinner from './Spinner';
 
 const JobListings = ({ isHome = false }) => {
-  const [jobs, setJobs] = useState(null);
+  const [jobs, setJobs] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
