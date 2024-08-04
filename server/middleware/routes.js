6b9cff646e623945e885665bcca4c359ce6d9da5
Aqeel Malik <malik.aqeelarshad@gmail.com>
2024-08-05 01:11:28 +0400

    get all routes

--- server/middleware/routes.js
+++ server/middleware/routes.js
@@ -2,14 +2,16 @@ const getRoutes = (app, req, res, next) => {
   const routes = []
   app._router.stack.map((r) => {
     if (r.name === 'router') {
-      const prefix =  r.regexp.toString().replace(/\/\^|\/\?|\/\$/g, '').replace('(?=\\/|$)', '').replace(/\\(.)/g, '$1').replace(/\/i\n/g, '').replace(/\/i$/, '');
+      const prefix = r.regexp.toString().replace(/\/\^|\/\?|\/\$/g, '').replace('(?=\\/|$)', '').replace(/\\(.)/g, '$1').replace(/\/i\n/g, '').replace(/\/i$/, '');
+      const url = req.protocol + '://' + req.get('host') + req.originalUrl + prefix.replace(/^\/|\/$/g, '');
       r.handle.stack?.map((r) => {
         const path = r.route?.path;
         r.route?.stack?.map((r) => {
           routes.push({
-            path: path,
             method: r.method,
-            prefix: prefix
+            path: path,
+            prefix: prefix,
+            url: (url + path).replace(/^\/|\/$/g, '')
           })
         })
       })
