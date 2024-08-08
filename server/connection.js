d1d3e96798712d799e439d7651d9ccb16420d57d
Aqeel Malik <malik.aqeelarshad@gmail.com>
2024-08-08 10:49:41 +0400

    connection.js updated for vercel

--- server/connection.js
+++ server/connection.js
@@ -8,7 +8,7 @@ const Database = (name) => {
 
 	if (connection.includes(dbName)) return database[dbName];
 
-	const uri = process.env[`REACT_APP_MONGODB_URI_${dbName}`];
+	const uri = process.env[`API_MONGODB_URI_${dbName}`];
 	console.log('uri', uri, dbName, connection);
 	try {
 		database[dbName] = mongoose.createConnection(uri);
