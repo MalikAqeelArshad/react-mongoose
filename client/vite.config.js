75321436dbe0f9f6a2f5875b5d8199e0cbddba5d
Aqeel Malik <malik.aqeelarshad@gmail.com>
2024-08-08 12:17:32 +0400

    vite.config.js updated for vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
+      cors: false,
       headers: { 'x-database' : 'REACT' },
       '/api': {
         target: 'https://react-mongoose-api.vercel.app',
